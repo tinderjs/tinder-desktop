3a1c13d60387d0554109eb8ded0aaf1c92530171
Some Guy <ts@thomasschneider.com>
2015-05-24 09:05:01 +0200

    Lets you see profile after match

---
+++ desktop-app/js/tinder++.profile.js
@@ -0,0 +1,14 @@
+(function() {
+  var module = angular.module('tinder++.profile', ['ngRoute', 'tinder++.api', 'tinder++.swipe']);
+
+  module.controller('ProfileController', function($scope, $routeParams, API) {
+    API.userInfo($routeParams.userId, function(_, data) {
+      $scope.user = data.results;
+    });
+
+    $scope.photoIndex = 0;
+    $scope.swapPhoto = function(index) {
+      $scope.photoIndex = index;
+    };
+  });
+})();
