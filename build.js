d8212364dcfc1019db00632b9a88faf1730b4104
Kyle Powers <kylepowers@gmail.com>
2015-06-02 02:30:43 -0700

    update build script

--- build.js
+++ build.js
@@ -1,12 +1,16 @@
 var NwBuilder = require('node-webkit-builder');
+var appPkg = require('./desktop-app/package.json');
+
 var nw = new NwBuilder({
   files: 'desktop-app/**',
-  platforms: ['osx', 'win', 'linux32', 'linux64'],
+  platforms: ['osx32', 'osx64', 'win32', 'win64'],
   version: '0.11.2',
   appName: 'Tinder⁺⁺',
-  appVersion: '1.0.0',
+  appVersion: appPkg.version,
   winIco: 'icons/win.ico',
-  macIcns: 'icons/mac.icns'
+  macIcns: 'icons/mac.icns',
+  buildType: 'timestamped',
+  macZip: true
 });
 
 nw.on('log', console.log);
