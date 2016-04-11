b0bc223d54c2248147a13d7ba57334b3c5c1b136
Stuart Williams <stuart@sidereal.ca>
2016-04-10 21:06:57 -0400

    #5 Remove metadata from build output nested package.json files

--- build.js
+++ build.js
@@ -2,6 +2,7 @@
 console.log('Before running, make sure versions are updated in both package.json and desktop-app/package.json');
 
 var NwBuilder = require('nw-builder');
+var removeNPMAbsolutePaths = require('removeNPMAbsolutePaths');
 var appPkg = require('./desktop-app/package.json');
 var appName = 'tinder-desktop';
 var buildDir = 'build/output';
@@ -24,6 +25,7 @@ nw.on('log', console.log);
 
 nw.build()
   .then(function () {
+    removeNPMAbsolutePaths(buildDir);
     console.log('done building apps');
     createDMG();
     createNW();
