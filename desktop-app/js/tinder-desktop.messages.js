8106e850fafc9e9117c3cffc74812eb722d15125
Anthony Mayfield <contact@anthonymayfield.com>
2016-04-29 21:10:42 -0700

    adding some better documentation

--- desktop-app/js/tinder-desktop.messages.js
+++ desktop-app/js/tinder-desktop.messages.js
@@ -4,32 +4,46 @@
   module.controller('MessagesController', function($scope, API, Settings) {
     // console.log(API.conversations)
     $scope.conversations = API.conversations;
+
+    // amount of matches
     $scope.conversationCount = Object.keys($scope.conversations).length
+
+    // show extra settings
     $scope.showExtra = Settings.get('messageListExtraInfo') === 'yes';
+
+    // open the conversation
     $scope.open = function(matchId) {
-      $scope.currentMatch = matchId
+      $scope.currentMatch = matchId;
       $scope.conversation = $scope.conversations[matchId];
     };
+
+    // map the word enter to 13 for keypress binding
     var ENTER = 13;
 
+    // trigger a swal alert and when confirmed, unmatch based on matchId
     $scope.unmatch = function(conversation){
-      swal({   
-        title: "Unmatch with " + conversation.name + "?",   
-        text: "You will not be able to message this person",   // Unmatch with conversation.name 
-        type: "info",   
-        showCancelButton: true,   
-        confirmButtonColor: "#DD6B55",   
-        confirmButtonText: "Yes, unmatch",   
-        closeOnConfirm: true }, 
-      function(){   
+      swal({
+        title: "Unmatch with " + conversation.name + "?",
+        text: "You will not be able to message this person",   // Unmatch with conversation.name
+        type: "info",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, unmatch",
+        closeOnConfirm: true },
+      function(){
         API.unmatch(conversation.matchId)
       });
-    }
+    };
 
+    // determine the color to show based on last message
     $scope.lastMessageClass = function (match) {
       if (match.messages.length) {
+        // select last message
         var lastMessage = match.messages[match.messages.length - 1];
+
+        // if last message is from me
         if (lastMessage.fromMe) {
+          // TODO: figure out the rest of these messages and their mapping to CSS
           if (moment(match.userPingTime).isAfter(lastMessage.sentDate)) {
             return 'last-me-pass';
           } else {
@@ -42,6 +56,8 @@
       return '';
     };
 
+    // TODO: Link into here for sending messages based on a click, copy the pending pattern
+    // capture enter keypresses and send the written message
     $scope.keypress = function(event) {
       if (event.which == ENTER) {
         event.preventDefault();
@@ -49,14 +65,27 @@
           API.sendMessage($scope.conversation.matchId, $scope.message);
           // Show pending message
           $scope.conversation.pending = $scope.conversation.pending || [];
+          // send the current message to pending conversations
           $scope.conversation.pending.push($scope.message);
-          // Reset
+          // Reset to an empty message
           $scope.message = '';
         }
       }
     };
-  });
 
+  //   $scope.selectMessage = function (event) {
+  //     // TODO: Fix: We don't want $scope.message here
+  //     API.sendMessage($scope.conversation.matchId, $scope.message);
+  //     // Show pending message
+  //     $scope.conversation.pending = $scope.conversation.pending || [];
+  //     // send the current message to pending conversations
+  //     $scope.conversation.pending.push($scope.message);
+  //     // Reset to an empty message
+  //     $scope.message = '';
+  //   }
+  // });
+
+  // Order objects by a certain field
   module.filter('orderObjectBy', function() {
     return function(items, field, reverse) {
       var filtered = [];
@@ -70,7 +99,8 @@
       return filtered;
     };
   });
-  
+
+  // TODO: Document this directive
   module.directive('shortTimeAgo', function($interval) {
     return {
       restrict: 'A',
@@ -98,6 +128,7 @@
       });
     }
 
+    // time formatter
     function minutesToShortTime (minutes) {
       var hours = Math.floor(minutes / 60);
       var days = Math.floor(minutes / 1440);
