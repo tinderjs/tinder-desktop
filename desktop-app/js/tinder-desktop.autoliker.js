ed3cfe3690fa6385981b91a4c7e6cf0a958ba7cf
Anthony <contact@anthonymayfield.com>
2016-10-22 16:26:52 -0700

    finishing up the feature

--- desktop-app/js/tinder-desktop.autoliker.js
+++ desktop-app/js/tinder-desktop.autoliker.js
@@ -12,23 +12,29 @@
       return diffYears
     }
 
-    $scope.likedGirls = [{name: 'Suzie', age: '23', distance: '8 km'}];
+    $scope.likedGirls = [];
+
+    $scope.allowedToAutolike = true;
+
+    $scope.stopAutoliking = function(){
+      $scope.allowedToAutolike = false;
+    };
 
     function likeGirls(){
 
       API.autoPeople(10).then(function(recResponse){
-        console.log(JSON.stringify(recResponse, 2, 2));
+        // console.log(JSON.stringify(recResponse, 2, 2));
         recResponse.results.forEach(function(girl, index){
           let timeout = index * 2000;
 
           setTimeout(function(){
             let age = getYearsOld(girl.birth_date);
             let name = girl.name;
-            let distance = (girl.distance_mi * 1.6)
+            let distance = (girl.distance_mi * 1.6).toFixed(1);
             let userId = girl._id;
 
             API.autoLike(girl._id).then(function(likeResponse){
-              console.log(JSON.stringify(likeResponse, 2, 2));
+              // console.log(JSON.stringify(likeResponse, 2, 2));
             });
 
             var girlObject = {
@@ -46,6 +52,7 @@
     }
 
     function reboot(){
+      if(!$scope.allowedToAutolike) return;
       // begin liking girls
       likeGirls();
       setTimeout(function(){
@@ -55,6 +62,8 @@
 
 
     $scope.autoLikeGirls = function(){
+      $scope.allowedToAutolike = true;
+
       reboot();
     }
 
